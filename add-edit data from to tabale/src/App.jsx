4d56baeab43b9915f520f49f263bcf42dcca9fd1
Sujal Chovatiya <sujalchovatiya.01@outlook.com>
2024-12-12 16:23:17 +0530

    Add files via upload

---
+++ add-edit data from to tabale/src/App.jsx
@@ -0,0 +1,16 @@
+import Eform from './component/Eform'
+// import Employee from './component/Employee'
+
+function App() {
+
+  return (
+    <>
+    <div>
+      {/* <Employee /> */}
+      <Eform />
+    </div>
+    </>
+  )
+}
+
+export default App
